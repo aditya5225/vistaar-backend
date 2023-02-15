cbb8da367e96a1047f6e0c27b1bbfb29fbbb11f5
Aditya <adityamaurya5225@gmail.com>
2023-02-15 08:36:55 +0530

    changes

--- controllers/accountsController.js
+++ controllers/accountsController.js
@@ -1,4 +1,5 @@
 const accountsManagerDb = require('../models/accountsManagerModel');
+const transactionsDb = require('../models/transactionsModel');
 
 const fetchAccountsFun = async (req, res) => {
     let response = {};
@@ -8,39 +9,47 @@ const fetchAccountsFun = async (req, res) => {
         const skipData = req.query && req.query.skip && req.query.skip >= 0 ? (req.query.skip - 1) * limitData : 0;
         let limitAmoutn = req.query && req.query.amount && req.query.amount >= 0 ? parseInt(req.query.amount) : 0
 
-        const accountsData = await accountsManagerDb.aggregate([
-            {
-                $lookup: {
-                    from: "transactions",
-                    localField: "account_id",
-                    foreignField: "account_id",
-                    as: "transactions"
-                }
-            },
-            { $unwind: '$transactions' },
-            { $match: { 'transactions.transactions': { $elemMatch: { amount: { $lt: limitAmoutn } } } } },
-            {
-                $replaceWith: {
-                    $unsetField: {
-                        field: "transactions",
-                        input: "$$ROOT"
-                    }
-                }
-            },
-            {
-                $facet: {
-                    accountsData: [{ $skip: +skipData }, { $limit: +limitData }],
-                    dataCount: [{ $count: 'count' }]
-                }
-            },
-            { $unwind: '$dataCount' },
-        ]);
+        // This is the simplest logic to achieve this.
+        const findQuery = limitAmoutn ? { transactions: { $elemMatch: { amount: { $lt: limitAmoutn } } } } : {};
+        const accountsData = await transactionsDb.find(findQuery).select("-transactions").skip(skipData).limit(limitData);
+        const dataCount = await transactionsDb.find(findQuery).count();
+
+        // ********* This is another logic with accounts collection and aggregate down below. Please uncomment it and fields inside response to test it. *********
+        // const accountsData = await accountsManagerDb.aggregate([
+        //     {
+        //         $lookup: {
+        //             from: "transactions",
+        //             localField: "account_id",
+        //             foreignField: "account_id",
+        //             as: "transactions"
+        //         }
+        //     },
+        //     { $unwind: '$transactions' },
+        //     ...(limitAmoutn ? [{ $match: { 'transactions.transactions': { $elemMatch: { amount: { $lt: limitAmoutn } } } } }] : []),
+        //     {
+        //         $replaceWith: {
+        //             $unsetField: {
+        //                 field: "transactions",
+        //                 input: "$$ROOT"
+        //             }
+        //         }
+        //     },
+        //     {
+        //         $facet: {
+        //             accountsData: [{ $skip: +skipData }, { $limit: +limitData }],
+        //             dataCount: [{ $count: 'count' }]
+        //         }
+        //     },
+        //     { $unwind: '$dataCount' },
+        // ]);
 
         response = {
             error: false,
             result: {
-                dataCount: accountsData && accountsData[0]?.dataCount?.count ? accountsData[0].dataCount.count : 0,
-                accountsData: accountsData && accountsData[0].accountsData ? accountsData[0].accountsData : [],
+                dataCount: dataCount,
+                accountsData: accountsData,
+                // dataCount: accountsData && accountsData[0]?.dataCount?.count ? accountsData[0].dataCount.count : 0,      // Please uncomment it to test aggregate logic.
+                // accountsData: accountsData && accountsData[0].accountsData ? accountsData[0].accountsData : [],
                 message: `Data fetched successfully!`
             }
         }
